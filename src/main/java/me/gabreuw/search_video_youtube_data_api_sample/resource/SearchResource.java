5a5be78d8de3595e3f6997f8f211fa97f90521e9
ImGabreuw <imgabreuw.dev@gmail.com>
2021-04-13 19:51:15 -0300

    Refactored code

--- src/main/java/me/gabreuw/search_video_youtube_data_api_sample/resource/SearchResource.java
+++ src/main/java/me/gabreuw/search_video_youtube_data_api_sample/resource/SearchResource.java
@@ -6,10 +6,7 @@ import me.gabreuw.search_video_youtube_data_api_sample.domain.VideoDTO;
 import me.gabreuw.search_video_youtube_data_api_sample.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/search")
@@ -18,9 +15,9 @@ public class SearchResource {
     @Autowired
     private SearchService searchService;
 
-    @GetMapping(path = "/videoId/{videoId}")
+    @GetMapping
     public ResponseEntity<VideoDTO> searchByVideoId(
-            @PathVariable String videoId
+            @RequestParam String videoId
     ) {
         Video video = searchService.searchVideoById(videoId);
 
@@ -30,8 +27,8 @@ public class SearchResource {
     }
 
     @GetMapping(path = "/{videoTitle}")
-    public ResponseEntity<VideoDTO> search(
-            @PathVariable String videoTitle
+    public ResponseEntity<VideoDTO> searchByVideoTitle(
+            @RequestParam String videoTitle
     ) {
         String videoId = searchService.searchByQuery(videoTitle).getId().getVideoId();
         Video video = searchService.searchVideoById(videoId);
